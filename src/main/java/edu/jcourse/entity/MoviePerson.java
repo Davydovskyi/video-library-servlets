2a04c5a1f95f25f81616d9c253bd7c9766765df8
Anton Davydovskyi <anton.davydoone@mail.ru>
2023-05-22 12:56:24 +0300

    refactoring: MoviePerson, Review

--- src/main/java/edu/jcourse/entity/MoviePerson.java
+++ src/main/java/edu/jcourse/entity/MoviePerson.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+import java.util.Optional;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -14,4 +17,38 @@ public class MoviePerson {
     private Movie movie;
     private Person person;
     private PersonRole personRole;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MoviePerson that = (MoviePerson) o;
+
+        if (!Objects.equals(id, that.id)) return false;
+        if (!Objects.equals(Optional.ofNullable(movie).map(Movie::getId).orElse(null), Optional.ofNullable(that.movie).map(Movie::getId).orElse(null)))
+            return false;
+        if (!Objects.equals(person, that.person)) return false;
+        return personRole == that.personRole;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (Optional.ofNullable(movie).map(Movie::getId).map(Object::hashCode).orElse(0));
+        result = 31 * result + (person != null ? person.hashCode() : 0);
+        result = 31 * result + (personRole != null ? personRole.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder(getClass().getSimpleName());
+        sb.append("{id=").append(id);
+        sb.append(", movieId=").append(Optional.ofNullable(movie).map(Movie::getId).orElse(null));
+        sb.append(", person=").append(person);
+        sb.append(", personRole=").append(personRole);
+        sb.append('}');
+        return sb.toString();
+    }
 }
--- src/main/java/edu/jcourse/entity/Review.java
+++ src/main/java/edu/jcourse/entity/Review.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+import java.util.Optional;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,4 +18,41 @@ public class Review {
     private User user;
     private String text;
     private Short rate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Review review = (Review) o;
+
+        if (!Objects.equals(id, review.id)) return false;
+        if (!Objects.equals(Optional.ofNullable(movie).map(Movie::getId).orElse(null), Optional.ofNullable(review.movie).map(Movie::getId).orElse(null)))
+            return false;
+        if (!Objects.equals(user, review.user)) return false;
+        if (!Objects.equals(text, review.text)) return false;
+        return Objects.equals(rate, review.rate);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (Optional.ofNullable(movie).map(Movie::getId).map(Object::hashCode).orElse(0));
+        result = 31 * result + (user != null ? user.hashCode() : 0);
+        result = 31 * result + (text != null ? text.hashCode() : 0);
+        result = 31 * result + (rate != null ? rate.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder(getClass().getSimpleName());
+        sb.append("{id=").append(id);
+        sb.append(", movieId=").append(Optional.ofNullable(movie).map(Movie::getId).orElse(null));
+        sb.append(", user=").append(user);
+        sb.append(", text='").append(text).append('\'');
+        sb.append(", rate=").append(rate);
+        sb.append('}');
+        return sb.toString();
+    }
 }
